7d4fc4c73dabb7234d3c4e51e2c15b25bd6031dc
nazarysagastumesanchez <138066301+nazarysagastumesanchez@users.noreply.github.com>
2023-06-28 15:25:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstname = "nazary"; 
+var lastname = "sagastume";
+var number = 4;
+var haspets = 0;
+
+console.log(firstname);
+console.log(lastname);
+console.log(number);
+console.log(haspets);
